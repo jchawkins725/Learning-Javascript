b0b1bd2cf97f9c5ff66ccabf2af43b0254f63657
Jonathan Hawkins <32544599+jchawkins725@users.noreply.github.com>
2019-02-25 15:35:07 -0500

    Update OrbitalPeriod.js

--- OrbitalPeriod.js
+++ OrbitalPeriod.js
@@ -1,4 +1,13 @@
 // change x to corresponding element
 
-//equation:
-Math.round(Math.sqrt(Math.pow(x.avgAlt + earthRadius, 3) / GM) * 2 * Math.PI)
+//equation: Math.round(Math.sqrt(Math.pow(x.avgAlt + earthRadius, 3) / GM) * 2 * Math.PI)
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  for (let i=0; i<arr.length; i++){
+  arr[i].orbitalPeriod = Math.round(Math.sqrt(Math.pow(arr[i].avgAlt + earthRadius, 3) / GM) * 2 * Math.PI);
+  delete arr[i].avgAlt;
+  }
+  return arr;
+}
