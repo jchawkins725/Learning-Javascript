16d63969826e132757b6a5111545ab268d2cb6dc
Jonathan Hawkins <32544599+jchawkins725@users.noreply.github.com>
2019-02-26 14:52:07 -0500

    Create caesars-ciphers.js

---
+++ caesars-ciphers.js
@@ -0,0 +1,15 @@
+function rot13(str) { // LBH QVQ VG!
+  let unicodeString = str.split("").map(x => x.charCodeAt());
+  for (let i=0; i<unicodeString.length; i++) {
+    if (unicodeString[i] >= 65 && unicodeString[i]<=77) {
+      unicodeString[i]+=13;
+    }
+    else if (unicodeString[i] >=78 && unicodeString[i]<=90) {
+      unicodeString[i]-=13;
+    }
+    else {
+      ;
+    }
+  }
+  return String.fromCharCode(...unicodeString)
+}
