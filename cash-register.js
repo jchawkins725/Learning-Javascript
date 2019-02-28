cc2b41307f754dad1b308c6c650b4400b6bc8432
Jonathan Hawkins <32544599+jchawkins725@users.noreply.github.com>
2019-02-28 00:08:17 -0500

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -8,29 +8,29 @@ function checkCashRegister(price, cash, cid) {
   var amounts = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 
   for (let i=currency.length-1; i>=0; i--) {
-    while (change >= currency[i]) {
-      cid[i][1] -= currency[i];
-      amounts[i][1]+=currency[i];
-      statusObj.status = "OPEN";
-      statusObj.change = [amounts[i]];
-      change -= currency[i];
+    while (change.toFixed(2) >= currency[i]) {
+      if (cid[i][1] <= 0) {
+        i--;
+      }
+      else {
+        amounts[i][1]+=currency[i];
+        cid[i][1] -= currency[i];
+        change -= currency[i];
       }
     }
-  //statusObj.status = "INSUFFICIENT_FUNDS";
-  //statusObj.change = [];
-  return statusObj;
-
+    }
+  if (change > 0) {
+    statusObj.status = "INSUFFICIENT_FUNDS";
+  }
+  else {
+    for (let j=0; j<amounts.length; j++) {
+       if (amounts[j][1] > 0) {
+          statusObj.change.unshift(amounts[j]);
+          statusObj.status = "OPEN";
+       }
+  }
 }
-
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
+  return statusObj;
+}
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
