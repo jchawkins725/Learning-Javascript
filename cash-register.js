b97260c21c697d450e47a1ee4b7aa4a10d63cb08
Jonathan Hawkins <32544599+jchawkins725@users.noreply.github.com>
2019-02-28 16:20:26 -0500

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -7,6 +7,16 @@ function checkCashRegister(price, cash, cid) {
   };
   var amounts = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 
+  var registerTotal = 0;
+  for (let x=0; x<cid.length; x++) {
+    registerTotal+=cid[x][1]
+  }
+  if (registerTotal === change) {
+    statusObj.status = "CLOSED"
+    statusObj.change = cid;
+    return statusObj;
+  }
+
   for (let i=currency.length-1; i>=0; i--) {
     while (change.toFixed(2) >= currency[i]) {
       if (cid[i][1] <= 0) {
@@ -18,19 +28,20 @@ function checkCashRegister(price, cash, cid) {
         change -= currency[i];
       }
     }
-    }
+  }
   if (change > 0) {
     statusObj.status = "INSUFFICIENT_FUNDS";
   }
+
   else {
     for (let j=0; j<amounts.length; j++) {
-       if (amounts[j][1] > 0) {
-          statusObj.change.unshift(amounts[j]);
-          statusObj.status = "OPEN";
+      if (amounts[j][1] > 0) {
+        statusObj.change.unshift(amounts[j]);
+        statusObj.status = "OPEN";
        }
-  }
-}
+    }
+  } 
   return statusObj;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
