8c259d0f79cdaadde26f5181cc7690044b317207
Jonathan Hawkins <jchawkins725@gmail.com>
2019-06-27 11:16:45 -0400

    more algorithims

---
+++ intermediate algorithms/mumbling.js
@@ -0,0 +1,21 @@
+/*
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+function accum(s) {
+    let count = 1;
+    let newString = "";
+    for (let i=0; i<s.length; i++) {
+      newString += s[i].toUpperCase();
+      for (let j=1; j<count; j++) {
+        newString += s[i].toLowerCase();
+      }
+      count++;
+      if (s[i] != s[s.length -1]) {
+        newString += "-";
+      }
+    }
+    return newString
+  }
